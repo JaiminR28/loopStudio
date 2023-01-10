fbeb8aa7ffa052a345de265181e6a758613cbb26
JaiminR28 <jaiminrathwa28@gmail.com>
2023-01-10 11:01:35 +0530

    building hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
 	content: ["./*.html"],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				sm: "480px",
+				md: "768px",
+				lg: "976px",
+				xl: "1440px",
+			},
+			fontFamily: {
+				sans: ["Josefin Sans", "sans-serif"],
+				Alata: ["Alata"],
+			},
+		},
 	},
 	plugins: [],
 };
